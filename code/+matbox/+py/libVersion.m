2622933e9da5b18e5f72b6cf031c49685d6982e0
ehennestad <ehennestad@gmail.com>
2024-10-30 13:13:46 +0100

    Change: Improve function in py namespace
    Main changes:
    - Add getPackageInfo function
    - Use "pip show" in system command to get package info
    - Add python package location to PYTHONPATH after pipInstall if it is not present from before

--- code/+matbox/+py/libVersion.m
+++
@@ -1,7 +0,0 @@
-function version = libVersion(packageName)
-    arguments
-        packageName (1,1) string
-    end
-
-    version = py.importlib.metadata.version(packageName);
-end
--- code/+matbox/+py/pipInstall.m
+++ code/+matbox/+py/pipInstall.m
@@ -1,8 +1,61 @@
 function pipInstall(packageName)
+% pipInstall - Install a specified Python package using pip from within MATLAB
+%
+% Syntax:
+%   pipInstall(packageName)
+%
+% Description:
+%   This function installs a specified Python package using `pip` from the
+%   Python executable configured in MATLAB. If the installation fails, an
+%   error is thrown. After installation, it checks if the install location
+%   is in the `PYTHONPATH` and adds it if necessary.
+%
+% Input:
+%   packageName - (string) The name of the Python package to install.
+%
+% Exceptions:
+%   Throws an error with ID 'MatBox:UnableToInstallPythonLibrary' if the
+%   package cannot be installed.
+%
+% Notes:
+%   Python must be set up and configured in MATLAB using `pyenv`, and `pip`
+%   must be available in the Python environment.
+%
+% Example:
+%   % Install the 'numpy' package
+%   matbox.py.pipInstall("numpy")
+%
+% See also:
+%   matbox.py.getPythonExecutable, matbox.py.getPackageInfo
+
     arguments
         packageName (1,1) string
     end
 
-    args = py.list({py.sys.executable, "-m", "pip", "install", packageName});
-    py.subprocess.check_call(args);
+    pythonExecutable = matbox.py.getPythonExecutable();
+
+    systemCommand = sprintf("%s -m pip install %s", pythonExecutable, packageName);
+    [status, ~]  = system(systemCommand);
+
+    if status ~= 0
+        error("MatBox:UnableToInstallPythonLibrary", ...
+            "Could not use pip to install %s", packageName)
+    end
+
+    % Add install location to PYTHONPATH if it is not already there
+    installLocation = matbox.py.getPackageInfo("pybadges", "Field", "Location");
+
+    checkAndUpdatePythonPath(installLocation, packageName) % Local function
+end
+
+function checkAndUpdatePythonPath(installLocation, packageName)
+
+    pyPath = py.sys.path();
+    pyPath = string(pyPath);
+    pyPath(pyPath=="") = [];
+
+    if ~any( contains(pyPath, installLocation) )
+        fprintf("Adding %s location to pythonpath\n", packageName)
+        py.sys.path().append(installLocation) 
+    end
 end
