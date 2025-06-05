9eb09d16d562f1893f50fe6d9d9a54b7a8794cc3
ehennestad <ehennestad@gmail.com>
2025-06-05 22:56:18 +0200

    Remove unused code in install-matbox

--- .github/actions/install-matbox/installMatBox.m
+++ .github/actions/install-matbox/installMatBox.m
@@ -1,15 +1,14 @@
-function installMatBox(mode, options)
+function installMatBox(mode)
 % installMatBox - Install MatBox from latest release or latest commit
 
     arguments
         mode (1,1) string {mustBeMember(mode, ["release", "commit"])} = "release"
-        options.InstallationLocation = userpath
     end
     
     if mode == "release"
         installFromRelease() % local function
     elseif mode == "commit"
-        installFromCommit(options.InstallationLocation) % local function
+        installFromCommit() % local function
     end
 end
 
@@ -35,38 +34,12 @@ function installFromRelease()
     end
 end
 
-
-function installFromCommit(installationLocation)
+function installFromCommit()
     
     scriptPath = mfilename('fullpath');
     projectFolder = extractBefore(scriptPath, fullfile('.github', 'actions'));
     codeDirectory = fullfile(projectFolder, 'code');
     
     addpath(genpath(codeDirectory))
-    savepath()
-    
-    return
-    
-    % Download latest zipped version of repo
-    url = "https://github.com/ehennestad/MatBox/archive/refs/heads/main.zip";
-    tempFilePath = websave(tempname, url);
-    cleanupObj = onCleanup(@(fp) delete(tempFilePath));
-    
-    % Unzip in temporary location
-    unzippedFiles = unzip(tempFilePath, tempdir);
-    unzippedFolder = unzippedFiles{1};
-    if endsWith(unzippedFolder, filesep)
-        unzippedFolder = unzippedFolder(1:end-1);
-    end
-    
-    % Move to installation location
-    [~, repoFolderName] = fileparts(unzippedFolder);
-    targetFolder = fullfile(installationLocation, "MATLAB Add-Ons", "Toolboxesss");
-    targetFolder = fullfile(targetFolder, repoFolderName);
-    if isfolder(targetFolder); rmdir(targetFolder, "s"); end
-    movefile(unzippedFolder, targetFolder);
-
-    % Add to MATLAB's search path
-    addpath(genpath(targetFolder))
     savepath()
 end
